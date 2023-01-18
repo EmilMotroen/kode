dcc576ab9b000d2fcc41a6549c8f9e0c1633c3c7
Emil Motrøen <e.motroen@gmail.com>
2023-01-18 09:58:27 +0100

    first commit

---
+++ write_data_to_db.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+"""
+Write data to an influxdb database
+"""
+from datetime import datetime
+import time
+
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+# You can generate a Token from the "Tokens Tab" in the UI
+token = "-CdLFPnsV_raxn3j6hYrp_u9pRg-1EGTAyPh047mlL6jfVWxTX_t3PZVBS2QWfjOJLSYatMcLtNjpFe47AmmLA=="
+org = "USN"
+bucket = "alpide-data"
+
+client = InfluxDBClient(url="http://localhost:8086", token=token)
+
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+x = 9
+y = -2
+z = -5
+i = 0
+points = []
+while i < 10:
+    point = Point("Coordinates")\
+        .tag("Location", "Horten")\
+        .field("x-coord", x)\
+        .field("y-coord", y)\
+        .field("z-coord", z)\
+        .time(datetime.utcnow(), WritePrecision.NS)
+    points.append(point)
+    
+    x -= 1
+    y += 1
+    z += 2
+    i += 1
+write_api.write(bucket, org, points)  # Skriver til database
+print('...ferdig')
