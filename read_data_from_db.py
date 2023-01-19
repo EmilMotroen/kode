97e4b5c4a9384c0781824b74bdef4818c364e43f
Emil Motrøen <e.motroen@gmail.com>
2023-01-19 13:00:29 +0100

    Kan bruke pandas til å hente info fra db

--- read_data_from_db.py
+++ read_data_from_db.py
@@ -1,10 +1,8 @@
 """
-Read data from influxdb database
+Read data from influxdb database using dataframes
 """
 import pandas as pd
-import matplotlib.pyplot as plt
-from influxdb_client import InfluxDBClient, Point, WritePrecision
-from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client import InfluxDBClient
 
 # You can generate a Token from the "Tokens Tab" in the UI
 token = "-CdLFPnsV_raxn3j6hYrp_u9pRg-1EGTAyPh047mlL6jfVWxTX_t3PZVBS2QWfjOJLSYatMcLtNjpFe47AmmLA=="
@@ -15,8 +13,13 @@ client = InfluxDBClient(url="http://localhost:8086", token=token, org=org)
 
 query_api = client.query_api()
 
-data_frame = query_api.query_data_frame('from(bucket:"alpide-data")'
-                                        '|> range(start: -12h)'
-                                        '|> pivot(rowKey:["_time"], columnKey:["_field"], valueColumn:"_value")')
-                                        #'|> keep(columns: ["x-coord", "y-coord", "z-coord"])')
+data_frame = query_api.query_data_frame(f'from(bucket:"{bucket}")'
+                                        '|> range(start: -36h)'
+                                        '|> pivot(rowKey:["_time"], columnKey:["_field"], valueColumn:"_value")'
+                                        '|> keep(columns: ["x-coord", "y-coord", "z-coord"])')
+
+pd_data_frame = pd.DataFrame(data_frame)
+print('query data frame:')
 print(data_frame.to_string())
+print('pandas data frame')
+print(pd_data_frame)
